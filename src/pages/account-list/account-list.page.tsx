88f06552f5e6ad518abdbcc0205ef74cf1c969be
Ainoa <aimmolina2@gmail.com>
2026-03-28 13:51:31 +0100

    ready to start with form component

--- src/pages/account-list/account-list.page.tsx
+++ src/pages/account-list/account-list.page.tsx
@@ -5,9 +5,12 @@ import classes from "./account-list.page.module.css";
 import { AccountListTableComponent } from "./components";
 import { mapAccountListFromApiToVm } from "./account-list.mapper";
 import { getAccountList } from "./api";
+import { useNavigate } from "react-router-dom";
+import { appRoutes } from "@/core/router";
 
 export const AccountListPage: React.FC = () => {
   const [accountList, setAccountList] = React.useState<AccountVm[]>([]);
+  const navigate = useNavigate();
 
   React.useEffect(() => {
     getAccountList().then((result) =>
@@ -15,12 +18,16 @@ export const AccountListPage: React.FC = () => {
     );
   }, []);
 
+  const handleAddNewAccount = () => {
+    navigate(appRoutes.createAccount);
+  };
+
   return (
     <AppLayout>
       <div className={classes.root}>
         <div className={classes.headerContainer}>
           <h1>Mis cuentas</h1>
-          <button>AGREGAR NUEVA CUENTA</button>
+          <button onClick={handleAddNewAccount}>AGREGAR NUEVA CUENTA</button>
         </div>
         <AccountListTableComponent accountList={accountList} />
       </div>
